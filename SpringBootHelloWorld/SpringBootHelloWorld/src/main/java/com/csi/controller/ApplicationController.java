1348abeac8bc1b460fc69c8a1973a6a7585ddf41
sunilingole <sunilingole1196@gmail.com>
2021-11-26 06:34:52 -0800

    changes done successfully

---
+++ SpringBootHelloWorld/SpringBootHelloWorld/src/main/java/com/csi/controller/ApplicationController.java
@@ -0,0 +1,28 @@
+package com.csi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ApplicationController {
+
+    @GetMapping
+    public String sayHello()
+    {
+        return "WELCOME TO CSI PUNE";
+    }
+
+    @GetMapping("/services")
+    public String csiServices()
+    {
+        return "SOFTWARE DEVELOPMENT";
+    }
+
+    @GetMapping("/address")
+    public String csiAddress()
+    {
+        return "INSPIRIA MALL | PUNE | MH | INDIA";
+    }
+}
